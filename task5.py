bc28999e255ff65c10b48c616eb9058da1a47941
OmegaLies <87187397+OmegaLies@users.noreply.github.com>
2022-11-15 01:07:17 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -48,7 +48,3 @@ def task(first_string, second_string):
             if S[i][j] == 0:
                 return False
     return True
-
-a = ["1", ["2","3"],"4", ["5", "6", "7"], "8", "9", "10"]
-b = [["1","2"], ["3","4","5"], "6", "7", "9", ["8","10"]]
-print(task(a,b))
